2a702e98bcf1f74e559d57f39a72c38e69a50937
phsequeira <phsequeira@yahoo.com>
2021-02-01 12:00:46 -0800

    fake data added to test table

--- data/player-pool.js
+++ data/player-pool.js
@@ -22,65 +22,66 @@
 //     img: ,
 // };
 
-const guards = {
-    id: 'DamonStoudamire',
-    cost: 1,
-    wins: 3,
-    position: '../assets/damsel.jpg',
-    img:,
-},
-{
-    id: 'DamienLillard',
-    cost: 3,
-    wins: 10,
-    position: '../assets/damsel.jpg',
-    img: ,
-}
-
- {   
-    id: 'KobeBryant',
-    cost: 3,
-    wins: 10,
-    position: '../assets/damsel.jpg',
-    img:
-},
-{
-    id: 'damsel',
-     wins: 'A Damsel in a high tower!',
-    position: '../assets/damsel.jpg',
-    img: ,
-};
-
-
-// const centers = {
-//     id: 'damsel',
+//const guards = {
+//    id: 'DamonStoudamire',
+//    cost: 1,
+//    wins: 3,
+//    position: '../assets/damsel.jpg',
+//    img:,
+//},
+//{
+//    id: 'DamienLillard',
+//    cost: 3,
+//    wins: 10,
+//    position: '../assets/damsel.jpg',
+//    img: ,
+//}
+//
+// {   
+//    id: 'KobeBryant',
+//    cost: 3,
+//    wins: 10,
+//    position: '../assets/damsel.jpg',
+//    img:
+//},
+//{
+//    id: 'damsel',
 //     wins: 'A Damsel in a high tower!',
-//     position: '../assets/damsel.jpg',
-//     img:,
-// },
-// {
-//     id: 'damsel',
-//     wins: 'A Damsel in a high tower!',
-//     position: '../assets/damsel.jpg',
-//     img: ,}
-
-//     {
-//         id: 'damsel',
-//         wins: 'A Damsel in a high tower!',
-//         position: '../assets/damsel.jpg',
-//         img:
-//     },
-//     {id: 'damsel',
-//     wins: 'A Damsel in a high tower!',
-//     position: '../assets/damsel.jpg',
-//     img: ,
-// };
-
-
-const players = [
-    // pointG, 
-    guards,
-    // centers,
-];
-
-export default players;
+//    position: '../assets/damsel.jpg',
+//    img: ,
+//};
+//
+//
+//// const centers = {
+////     id: 'damsel',
+////     wins: 'A Damsel in a high tower!',
+////     position: '../assets/damsel.jpg',
+////     img:,
+//// },
+//// {
+////     id: 'damsel',
+////     wins: 'A Damsel in a high tower!',
+////     position: '../assets/damsel.jpg',
+////     img: ,}
+//
+////     {
+////         id: 'damsel',
+////         wins: 'A Damsel in a high tower!',
+////         position: '../assets/damsel.jpg',
+////         img:
+////     },
+////     {id: 'damsel',
+////     wins: 'A Damsel in a high tower!',
+////     position: '../assets/damsel.jpg',
+////     img: ,
+//// };
+//
+//
+//const players = [
+//    // pointG, 
+//    guards,
+//    // centers,
+//];
+//
+//export default players;
+//
