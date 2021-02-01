2bd6ae6221e7a37faf3a7c9c84707b675d6733fb
Eion Nelson <ecnelson1@live.com>
2021-02-01 13:32:48 -0800

    updated player cost and wins

--- data/player-pool.js
+++ data/player-pool.js
@@ -5,19 +5,19 @@ const guards = {
     players: [{
         id: 'DamonStoudamire',
         cost: 1,
-        wins: 3,
+        wins: 55.4,
         position: 'PG',
         img: './assets/DamonStoudamire.PNG',
     }, {
         id: 'DamienLillard',
         cost: 2,
-        wins: 10,
+        wins: 84.8,
         position: 'PG',
         img: './assets/DamianLillard.PNG',
     }, {   
         id: 'KobeBryant',
-        cost: 2,
-        wins: 10,
+        cost: 3,
+        wins: 172.7,
         position: 'PG',
         img: './assets/KobeBryant.PNG',
     }]
@@ -29,20 +29,20 @@ const forwards = {
     description: 'The Forwards are you mid range shooters that help keep the ball moving, fast and agile.',
     players: [{
         id: 'VinceCarter',
-        cost: 2,
-        wins: 3,
+        cost: 3,
+        wins: 125.3,
         position: 'SF',
         img: './assets/VC.png',
     }, {
         id: 'TracyMcgrady',
         cost: 2,
-        wins: 5,
+        wins: 97.3,
         position: 'SF',
         img: './assets/TMac.png',
     }, {   
         id: 'LebronJames',
-        cost: 3,
-        wins: 7,
+        cost: 4,
+        wins: 239.4,
         position: 'SF',
         img: './assets/LbronJ.png',
     }]
@@ -54,20 +54,20 @@ const centers = {
     description: 'The Center is your size down low, close to the hoop, maybe free throw distance, but you want them to be a presence inside the paint.',
     players: [{
         id: 'KwameBrown',
-        cost: 1,
-        wins: 3,
+        cost: 2,
+        wins: 20.8,
         position: 'C',
         img: './assets/kwameB.png',
     }, {
         id: 'GregOden',
-        cost: 2,
-        wins: 1,
+        cost: 1,
+        wins: 7.3,
         position: 'C',
         img: './assets/gregO.png',
     }, {   
         id: 'YaoMing',
         cost: 3,
-        wins: 4,
+        wins: 65.9,
         position: 'C',
         img: './assets/YaoM.png',
     }]
