d8a410d78d9b0f60bdcfd132a25f136683f26e9e
Eion Nelson <ecnelson1@live.com>
2021-02-01 16:22:11 -0800

    working on script to render player pool to draft pages by postition

--- draft-page/players.js
+++ draft-page/players.js
@@ -1,13 +1,28 @@
-import { players } from '../player-pool.js';
-import { findByPosition } from '../utils.js';
-import { renderTable } from '../position-pages/render-table.js';
+import { players } from '../data/player-pool.js';
+import { findById } from '../utils.js';
+const PostionTitle = document.querySelector('h1');
+const draftDesc = document.querySelector('p');
+const userPick = document.querySelector('form');
+const draftPosition = new URLSearchParams(window.location.search);
+const PositionId = draftPosition.get('id');
+const draftPool = findById(players, PositionId);
+PostionTitle.textContent = draftPool.position;
+draftDesc.textContent = `Draft Your Player`;
 
-//empty div to send list data
-const draftablePlayers = document.getElementById('draft-pool');
-//loop through books array
-for (const player of players){
-    const newPlayer = findByPosition(players, player.position);
-    const draftable = renderTable(players, newPlayer);
+const draftablePlayers = document.getElementById('player-pool');
+
+for (let choice of draftablePlayers.id) {
+    const selection = document.createElement('input');
+    const label = document.createElement('label');
+    const playerName = document.createElement('p');
+    const playerPic = document.createElement('img');
+    playerName.textContent = choice.id;
     
-    draftablePlayers.appendChild(draftable);
+    selection.type = 'radio';
+    selection.value = choice.id;
+    selection.name = 'Decision';
+
+    label.append(playerName, playerPic, selection);
+
+    userPick.append(label);    
 }
