f027f6aad8e1ba5162f9f74b699e96dd6711a8d0
Eion Nelson <ecnelson1@live.com>
2021-02-02 05:59:51 -0800

    pushing up the draft pages that now fully render so team may work on finishing the  event listener for draft submission

--- draft-page/players.js
+++ draft-page/players.js
@@ -1,28 +1,38 @@
 import { players } from '../data/player-pool.js';
 import { findById } from '../utils.js';
-const PostionTitle = document.querySelector('h1');
 const draftDesc = document.querySelector('p');
 const userPick = document.querySelector('form');
 const draftPosition = new URLSearchParams(window.location.search);
 const PositionId = draftPosition.get('id');
 const draftPool = findById(players, PositionId);
-PostionTitle.textContent = draftPool.position;
 draftDesc.textContent = `Draft Your Player`;
-
-const draftablePlayers = document.getElementById('player-pool');
 
 for (let choice of draftPool.players) {
     const selection = document.createElement('input');
     const label = document.createElement('label');
     const playerName = document.createElement('p');
     const playerPic = document.createElement('img');
-    playerName.textContent = choice.id;
+    playerName.textContent = choice.id + ` $${choice.cost}`;
     
     selection.type = 'radio';
     selection.value = choice.id;
-    selection.name = 'Decision';
-
+    selection.name = 'drafted';
+    playerPic.src = choice.img;
     label.append(playerName, playerPic, selection);
 
     userPick.append(label);    
 }
+userPick.addEventListener('submit', (action) => {
+    action.preventDefault();
+    const data = new FormData(userPick);
+    const chosenPlayer = data.get('drafted');
+
+
+    directUser(userFunds);
+});
+    
+
+function directUser(userFunds){
+    if (userFunds <= 0){
+        setTimeout(function(){window.location = '../Results';}, 5000);
+    } else {setTimeout(function(){window.location = '../position-pages';}, 5000);}}
