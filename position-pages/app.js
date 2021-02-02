d039f5de3b1abed42682c9a66c68192fab249340
phsequeira <phsequeira@yahoo.com>
2021-02-02 13:42:47 -0800

    render table with localstorage data

--- position-pages/app.js
+++ position-pages/app.js
@@ -1,12 +1,8 @@
 import { renderUl } from './render-ul.js';
 import { header } from '../utils.js';
 import { renderTable } from './render-table.js';
-//import people from './fake-data.js';
-//import { getPlayer } from '../utils.js';
-import { getPlayer } from '../utils.js';
-const get = getPlayer();
-//const table = getPlayer();
+
 
 header();
 renderUl();
-renderTable(get);
+renderTable();
