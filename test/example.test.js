4c9a608817d492bc28ea195b76797a4d6cc28b27
Eion Nelson <ecnelson1@live.com>
2021-02-04 15:55:53 -0800

    fixed tests after data modifications

--- test/example.test.js
+++ test/example.test.js
@@ -15,53 +15,60 @@ test('locates item by it id characteristics and returns that item.', (expect) =>
         players: [{
             id: 'Damon Stoudamire',
             cost: 1,
-            wins: 4,
+            wins: 8,
             position: 'PG',
             img: '../assets/DamonStoudamire.PNG',
+            posWorth: 37, 
         }, {
             id: 'Victor Oladipo',
             cost: 1,
-            wins: 5,
+            wins: 9,
             position: 'SG',
             img: '../assets/vo.PNG',
+            posWorth: 78,
         }, {
             id: 'CJ McCollum',
             cost: 2,
-            wins: 8,
+            wins: 13,
             position: 'SG',
             img: '../assets/cj.PNG',
+            posWorth: 78,
         }, {
             id: 'Kyle Lowry',
             cost: 2,
-            wins: 7,
+            wins: 12,
             position: 'PG',
             img: '../assets/kl.PNG',
+            posWorth: 37,
         }, {
             id: 'Manu Ginobili',
             cost: 3,
-            wins: 11,
+            wins: 16,
             position: 'SG',
             img: '../assets/mg.PNG',
+            posWorth: 78,
         }, {
             id: 'Damien Lillard',
             cost: 3,
-            wins: 12,
+            wins: 17,
             position: 'PG',
             img: '../assets/DamianLillard.PNG',
+            posWorth: 37,
         }, {
             id: 'Steph Curry',
             cost: 4,
-            wins: 15,
+            wins: 19,
             position: 'PG',
-            img: '../assets/sc.PNG'
+            img: '../assets/sc.PNG',
+            posWorth: 37,
         }, {   
             id: 'Kobe Bryant',
             cost: 4,
-            wins: 16,
+            wins: 20,
             position: 'SG',
             img: '../assets/KobeBryant.PNG',
-        }]
-    };
+            posWorth: 78,
+        }] };
     
     
     const actual = findById(players, 'Guards');
@@ -73,8 +80,8 @@ test('Return apprpriate message based on funds and wins.', (expect) => {
     // if there are 2 or more funds and 20 or more ins the message should be 'superstar'
     //if the funds are 1 and the wins are 10 or more the message should be 'pro'
     //otherwise the message is 'rookie'
-    const wins = 21;
-    const expected = 'Your team had over 20 wins! You are a superstar!!';
+    const wins = 61;
+    const expected = 'Your team had over 60 wins! You are a superstar!!';
     const actual = createMessage(wins);
     expect.equal(actual, expected);
 });
