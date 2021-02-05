528a62892c561f20872f75f2298ac235e93536ce
Eion Nelson <ecnelson1@live.com>
2021-02-05 09:10:00 -0800

    removed empty files and whitespace as well as excess comments

--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,8 @@
-// IMPORT MODULES under test here:
+
 import { players } from '../data/player-pool.js';
 import { findById } from '../utils.js';
 import { createMessage, bonusMessage } from '../results/msg-utils.js';
 
-// import { example } from '../example.js';
 
 const test = QUnit.test;
 
@@ -77,9 +76,6 @@ test('locates item by it id characteristics and returns that item.', (expect) =>
 
 test('Return apprpriate message based on funds and wins.', (expect) => {
 
-    // if there are 2 or more funds and 20 or more ins the message should be 'superstar'
-    //if the funds are 1 and the wins are 10 or more the message should be 'pro'
-    //otherwise the message is 'rookie'
     const wins = 61;
     const expected = 'Your team had over 60 wins! You are a superstar!!';
     const actual = createMessage(wins);
